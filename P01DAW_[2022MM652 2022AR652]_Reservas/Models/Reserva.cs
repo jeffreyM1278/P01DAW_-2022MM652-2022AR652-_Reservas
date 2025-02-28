94ca722d091ec9c50c9b67ff06bae43a93773730
jeffr <jeffr@LAPTOP-NS6Q6ARS>
2025-02-27 20:41:02 -0600

    mani

--- P01DAW_[2022MM652 2022AR652]_Reservas/Models/Reserva.cs
+++ P01DAW_[2022MM652 2022AR652]_Reservas/Models/Reserva.cs
@@ -4,17 +4,14 @@ namespace P01DAW__2022MM652_2022AR652__Reservas.Models
 {
     public class Reserva
     {
-        public class Reserva
-        {
-            [Key] // Define ReservaId como Primary Key
-            public int ReservaId { get; set; }
+        [Key] // Define ReservaId como Primary Key
+        public int ReservaId { get; set; }
 
-            public int UsuarioId { get; set; }
-            public int EspacioId { get; set; }
-            public DateTime Fecha { get; set; }
-            public TimeSpan HoraInicio { get; set; }
-            public int CantidadHoras { get; set; }
-            public bool Activa { get; set; }
-        }
+        public int UsuarioId { get; set; }
+        public int EspacioId { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public int CantidadHoras { get; set; }
+        public bool Activa { get; set; }
     }
 }
