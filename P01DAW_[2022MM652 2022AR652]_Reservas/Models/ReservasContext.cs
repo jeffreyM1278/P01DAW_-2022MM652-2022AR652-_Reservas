ccfad2900d2ee032a11b65c2854ca4395f4a77a8
MiguelRamos-145 <miguel.arevalo@catolica.edu.sv>
2025-02-27 18:32:45 -0600

    creacion de modelos

--- P01DAW_[2022MM652 2022AR652]_Reservas/Models/ReservasContext.cs
+++ P01DAW_[2022MM652 2022AR652]_Reservas/Models/ReservasContext.cs
@@ -1,6 +1,37 @@
-﻿namespace P01DAW__2022MM652_2022AR652__Reservas.Models
+﻿namespace P01DAW__2022MM652_2022AR652__Reservas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+public class ReservasContext : DbContext
 {
-    public class ReservasContext
+    public ReservasContext(DbContextOptions<ReservasContext> options) : base(options) { }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Sucursal> Sucursales { get; set; }
+    public DbSet<EspacioParqueo> EspaciosParqueo { get; set; }
+    public DbSet<Reserva> Reservas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Usuario>()
+            .HasOne<Sucursal>()
+            .WithMany()
+            .HasForeignKey(u => u.SucursalId);
+
+        modelBuilder.Entity<EspacioParqueo>()
+            .HasOne<Sucursal>()
+            .WithMany()
+            .HasForeignKey(e => e.SucursalId);
+
+        modelBuilder.Entity<Reserva>()
+            .HasOne<Usuario>()
+            .WithMany()
+            .HasForeignKey(r => r.UsuarioId);
+
+        modelBuilder.Entity<Reserva>()
+            .HasOne<EspacioParqueo>()
+            .WithMany()
+            .HasForeignKey(r => r.EspacioId);
     }
 }
